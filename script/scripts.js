5a839581ea113d372b495a2ab872016681824e87
Diego Barajas <diegobarajas102030@gmail.com>
2024-02-10 22:55:38 -0500

    section skills and projects

--- script/scripts.js
+++ script/scripts.js
@@ -52,3 +52,41 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+function copyEmailToClipboard(){
+    let email = 'diegobarajas102030@gmail.com';
+    navigator.clipboard.writeText(email).then(function() {
+        // Notify the user that the text has been copied
+        var toastEmailEl = document.getElementById('emailToast');
+        var toastEmail = new bootstrap.Toast(toastEmailEl);
+
+        toastEmail.show();
+        updateToastEmail(5);
+
+    })
+    .catch(function(err) {
+        // Handle any errors that may occur
+        console.error('Could not copy email: ', err);
+    });;
+}
+
+
+function updateToastEmail(counter){
+    // Get the label element
+    if (counter != 0){
+        var label = document.getElementById('counterToast');
+        // Update the label text with the current time
+        label.textContent = counter;
+        console.log("counter : " + counter);
+        counter = counter -1;
+        // Call updateLabel function every second (1000 milliseconds)
+        waitForOneSecond().then(() => {
+            updateToastEmail(counter)
+        })
+    }
+}
+
+async function waitForOneSecond() {
+    await new Promise(resolve => setTimeout(resolve, 1000)); // 1000 milliseconds
+}
